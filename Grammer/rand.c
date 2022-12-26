92112dd8c4b9755b656e60d1693cbddff1b1c33e
hyeg <hyeg0121@naver.com>
2022-12-26 15:25:27 +0900

    (문법) 난수 발생

---
+++ Grammer/rand.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(void) {
+	
+	int arr[5];
+
+	srand(time(0));
+
+	for (int i = 0; i < 5; i++) {
+		arr[i] = (rand()%100)+1;
+		printf("%d ", arr[i]);
+	}
+
+	return 0;
+}
