566f4c525fd77ed398cf4648a175ff8211470aea
hyeg <hyeg0121@naver.com>
2022-12-26 14:35:01 +0900

    enemy 생성

--- AfterSchool/main.cpp
+++ AfterSchool/main.cpp
@@ -11,6 +11,12 @@ int main(void) {
 	player.setSize(Vector2f(40, 40));
 	player.setPosition(100, 100);
 	player.setFillColor(Color::Red);
+	int player_speed = 5; //플레이어의 움직임 속도
+
+	RectangleShape enemy;
+	enemy.setSize(Vector2f(70, 70));
+	enemy.setPosition(500, 300);
+	enemy.setFillColor(Color::Yellow);
 	
 	while (window.isOpen())//윈도우가 열려 있을 때 까지 창 유지 
 	{
@@ -31,17 +37,21 @@ int main(void) {
 		
 		//방향키를 눌렀을 때 플레이어 움직임
 		if (Keyboard::isKeyPressed(Keyboard::Left)) {
-			player.move(-1, 0);
-		}else if (Keyboard::isKeyPressed(Keyboard::Up)) {
-			player.move(0, -1);
-		}else if (Keyboard::isKeyPressed(Keyboard::Right)) {
-			player.move(1, 0);
-		}else if (Keyboard::isKeyPressed(Keyboard::Down)) {
-			player.move(0, 1);
+			player.move(-player_speed, 0);
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Up)) {
+			player.move(0, -player_speed);
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Right)) {
+			player.move(player_speed, 0);
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Down)) {
+			player.move(0, player_speed);
 		}
 
 		window.clear();
 		window.draw(player);
+		window.draw(enemy);
 		window.display();
 		
 	}//while
