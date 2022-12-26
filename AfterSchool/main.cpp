11ba73b1e628dda87755fdf88abf0d350eea9be7
hyeg <hyeg0121@naver.com>
2022-12-26 14:19:18 +0900

    플레이어 움직임 추가

--- AfterSchool/main.cpp
+++ AfterSchool/main.cpp
@@ -5,6 +5,7 @@ using namespace sf;
 int main(void) {
 
 	RenderWindow window(VideoMode(640, 480), "AfterSchool"); // 윈도우 창 생성
+	window.setFramerateLimit(60);
 
 	RectangleShape player;
 	player.setSize(Vector2f(40, 40));
@@ -27,9 +28,22 @@ int main(void) {
 			}
 
 		}//while
+		
+		//방향키를 눌렀을 때 플레이어 움직임
+		if (Keyboard::isKeyPressed(Keyboard::Left)) {
+			player.move(-1, 0);
+		}else if (Keyboard::isKeyPressed(Keyboard::Up)) {
+			player.move(0, -1);
+		}else if (Keyboard::isKeyPressed(Keyboard::Right)) {
+			player.move(1, 0);
+		}else if (Keyboard::isKeyPressed(Keyboard::Down)) {
+			player.move(0, 1);
+		}
 
+		window.clear();
 		window.draw(player);
 		window.display();
+		
 	}//while
 
 	return 0;
