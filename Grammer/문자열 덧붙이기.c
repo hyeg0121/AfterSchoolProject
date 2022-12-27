aec69103b09833c0ee86d6bd034c38bda9856151
hyeg <hyeg0121@naver.com>
2022-12-27 10:48:57 +0900

    (문법)문자열 덧붙이기3

--- Grammer/문자열 덧붙이기.c
+++ Grammer/문자열 덧붙이기.c
@@ -1,20 +1,14 @@
 #include<stdio.h>
-//#define _CRT_SECURE_NO_WARNINGS 
+#define _CRT_SECURE_NO_WARNINGS 
 
-int main(void) {
+int main(void) 
+{
 	char str1[30] = "score : ";
 	char str2[30] = "300";
-	int score = 300;
-
-	int i = strlen(str1), j = 0;
-
-	while (str2[j] != '\0') {
-		str1[i] = str2[j];
-		i++, j++;
-	}//str1 에 str2붙이기
-
-	str1[i] = '\0'; //마지막에 null
 	
+	strcat(str1, str2);
+
 	printf("%s", str1);
+
 	return 0;
 }
