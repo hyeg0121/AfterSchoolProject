6c2788f6b8b8cb3fc0f8f9d44ffd549973411b3b
hyeg <hyeg0121@naver.com>
2022-12-27 10:36:28 +0900

    윈도우에 글자 적용하기

--- AfterSchool/main.cpp
+++ AfterSchool/main.cpp
@@ -12,14 +12,27 @@ int main(void) {
 
 	srand(time(0));
 
+	Font font; 
+	font.loadFromFile("C:\\windows\\Fonts\\arial.ttf");
+
+	Text text;
+	text.setFont(font);
+	text.setCharacterSize(50);
+	text.setFillColor(Color(255, 255, 255));
+	text.setPosition(0, 0);
+	text.setString("score");
+
 	RectangleShape player;
 	player.setSize(Vector2f(40, 40));
 	player.setPosition(100, 100);
 	player.setFillColor(Color::Red);
 	int player_speed = 5; //플레이어의 움직임 속도
+	int player_score = 0; //플레이어의 점수
+
 
 	RectangleShape enemy[5];
 	int enemy_life[5];
+	int enemy_score = 100; //enemy를 잡았을 때 점수
 	for (int i = 0; i < 5; i++) {
 		enemy[i].setSize(Vector2f(70, 70));
 		enemy[i].setPosition(rand()%300+300, rand()%385);
@@ -79,20 +92,26 @@ int main(void) {
 				if (player.getGlobalBounds().intersects(enemy[i].getGlobalBounds())) {
 					printf("enemy[%d]와 충돌\n", i);
 					enemy_life[i] -= 1;
+					player_score += enemy_score;
 				}
 			}
 		}
 
+		printf("score : %d\n", player_score);
+
 		window.clear(Color::Black);
 
-		for (int i = 0; i < 5; i++)
+		for (int i = 0; i < 5; i++) //enemy가 살아있을때만 draw
 		{
 			if (enemy_life[i] > 0) {
 				window.draw(enemy[i]);
 			}
 		}
 
+		//draw
 		window.draw(player);
+		window.draw(text);
+
 		window.display();
 		
 	}//while
