6fd6189c64fc93939c4f6929ecd1eebddf65a98a
hyeg <hyeg0121@naver.com>
2022-12-27 10:45:29 +0900

    (문법)문자열 덧붙이기2

--- Grammer/문자열 덧붙이기.c
+++ Grammer/문자열 덧붙이기.c
@@ -5,13 +5,8 @@ int main(void) {
 	char str1[30] = "score : ";
 	char str2[30] = "300";
 	int score = 300;
-	char score_str[100];
 
-	int i = 0, j = 0;
-
-	while (str1[i] != '\0') {
-		i++;
-	}//str1의 끝 인덱스 찾기 
+	int i = strlen(str1), j = 0;
 
 	while (str2[j] != '\0') {
 		str1[i] = str2[j];
@@ -20,28 +15,6 @@ int main(void) {
 
 	str1[i] = '\0'; //마지막에 null
 	
-	//strcat(str1, str2);
 	printf("%s", str1);
 	return 0;
 }
-
-//char str[30] = "score : ";
-//// str의 글자 수 구하기
-//int str_len = 0;
-//while (str[str_len] != '\0')
-//str_len++;
-//
-//char str2[30] = "300";
-//// str2의 글자 수 구하기
-//int str2_len = 0;
-//while (str[str_len] != '\0')
-//str2_len++;
-//
-//for (int i = 0; i < str2_len; i++)
-//	str[i + str_len] = str2[i];
-//str[str_len + str2_len] = '\0'; // 문자의 끝엔 항상 null 값
-//printf("%s", str);
-//
-//int score = 300;
-//
-//printf("%s\n", score);
