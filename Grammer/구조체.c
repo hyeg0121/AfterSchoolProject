cbadbe6be7e95867ac1912398d0659a508c314bb
hyeg <hyeg0121@naver.com>
2022-12-27 14:36:43 +0900

    (문법)구조체 적용 후

--- Grammer/구조체.c
+++ Grammer/구조체.c
@@ -1,13 +1,22 @@
 #include<stdio.h>
 
+struct Saram
+{
+	int hp;
+	int speed;
+};
+
 int main(void)
 {
-	int player_hp = 5; 
-	int player_speed = 5;
+	struct Saram player;
 
-	int player_hp = 2;
-	int player_speed = 5;
+	player.hp = 5; 
+	player.speed = 5;
 
-	printf("플레이어의 속도 : %d 플레이어 속도 : %d\n", player_hp, player_speed);
+	struct Saram enemy;
+	enemy.hp = 2;
+	enemy.speed = 5;
+
+	printf("플레이어의 속도 : %d 플레이어 속도 : %d\n", player.hp, player.speed);
 	return 0;
 }
