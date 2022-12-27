d1eea9fa9fa829caee8705c2f9fcc93f43e783ed
hyeg <hyeg0121@naver.com>
2022-12-27 11:15:28 +0900

    (문법)문자열 덧붙이기4

--- Grammer/문자열 덧붙이기.c
+++ Grammer/문자열 덧붙이기.c
@@ -1,11 +1,17 @@
 #include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
 #define _CRT_SECURE_NO_WARNINGS 
 
 int main(void) 
 {
 	char str1[30] = "score : ";
-	char str2[30] = "300";
-	
+	int score = 300;
+
+	char str2[30];
+	itoa(score, str2, 10);
+
 	strcat(str1, str2);
 
 	printf("%s", str1);
