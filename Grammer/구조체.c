41a3daff8a49d8a979843eabf080b3fb11d62cdf
hyeg <hyeg0121@naver.com>
2022-12-27 14:32:27 +0900

    (문법)구조체 적용하기

---
+++ Grammer/구조체.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int player_hp = 5; 
+	int player_speed = 5;
+
+	int player_hp = 2;
+	int player_speed = 5;
+
+	printf("플레이어의 속도 : %d 플레이어 속도 : %d\n", player_hp, player_speed);
+	return 0;
+}
